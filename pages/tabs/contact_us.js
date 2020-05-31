a9c7d348a77d681e4de35dc20778636ea7860964
lethrez <leo.hoare@live.com>
2020-05-31 11:33:09 +1000

    maxwidth property for buttons

--- pages/tabs/contact_us.js
+++ pages/tabs/contact_us.js
@@ -29,20 +29,19 @@ export default class ContactUs extends Component {
                             <a  href="https://twitter.com/dataengconfau">
                                 <TwitterOutlined style={{fontSize:100, color: '#38A1F3', paddingRight:'50px'}}/>
                             </a>
-                            <Button href="https://twitter.com/dataengconfau" className='text-3xl'style={{wordWrap: 'break-word', whiteSpace: 'normal'}}>@dataengconfau</Button><br/>
-
+                            <Button href="https://twitter.com/dataengconfau" className='text-3xl'style={{wordWrap: 'break-word', whiteSpace: 'normal', maxWidth:'100%'}}>@dataengconfau</Button><br/>
                         </List.Item>
                         <List.Item>
                             <a href="https://www.linkedin.com/company/dataengconfau/about/">
                                 <LinkedinOutlined style={{fontSize:100, color: '#0e76a8', paddingRight:'50px'}}/>
                             </a>
-                            <Button href="https://www.linkedin.com/company/dataengconfau/about/" className='text-3xl' style={{wordWrap: 'break-word', whiteSpace: 'normal'}}>/dataengconfau</Button><br/>
+                            <Button href="https://www.linkedin.com/company/dataengconfau/about/" className='text-3xl' style={{wordWrap: 'break-word', whiteSpace: 'normal', maxWidth:'100%'}}>/dataengconfau</Button><br/>
                         </List.Item>
                         <List.Item>
                             <a href="mailto:sydney@dataengconf.com.au?Subject=DataEngConf" target="_top">
                                 <MailOutlined style={{fontSize:100, color: '#D44638', paddingRight:'50px'}}/>
                             </a>                      
-                            <Button href="mailto:sydney@dataengconf.com.au?Subject=DataEngConf" className='text-3xl' style={{wordWrap: 'break-word', whiteSpace: 'normal', maxWidth:'80%'}}>sydney@dataengconf.com.au</Button><br/>
+                            <Button href="mailto:sydney@dataengconf.com.au?Subject=DataEngConf" className='text-3xl' style={{wordWrap: 'break-word', whiteSpace: 'normal', maxWidth:'100%'}}>sydney@dataengconf.com.au</Button><br/>
                         </List.Item>
                     </List>
             </PageWrapper>
