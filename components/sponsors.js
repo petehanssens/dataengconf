cef9bfa98689a80f2ba35b9c07dad66d36fc756f
lethrez <leo.hoare@live.com>
2020-06-03 18:37:18 +1000

    add sponsor logos

--- components/sponsors.js
+++ components/sponsors.js
@@ -1,75 +1,70 @@
-import { Row, Col, Divider} from 'antd';
+import { List } from 'antd';
+
+function shuffleArray(array) {
+    let i = array.length - 1;
+    for (; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    return array;
+}
+
 
 const sponsors = [
     {
-        name: '1',
-        logo: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
+        name: 'cloudshuttle',
+        logo: "./images/sponsors/cloudshuttle.png",
     },
     {
-        name: '2',
-        logo: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
+        name: 'mongodb',
+        logo: "./images/sponsors/mongoDB_transparent.png",
     },
     {
-        name: '3',
-        logo: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
+        name: 'fivetran',
+        logo: "./images/sponsors/fivetran.png",
     },
     {
-        name: '4',
-        logo: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
+        name: 'snowflake',
+        logo: "./images/sponsors/snowflake.png",
     },
     {
-        name: '5',
-        logo: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
+        name: 'yow',
+        logo: "./images/sponsors/yow.png",
     },
-    {
-        name: '6',
-        logo: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
-    },
-    {
-        name: '7',
-        logo: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
-    },
-    {
-        name: '8',
-        logo: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
-    },
-    {
-        name: '9',
-        logo: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
-    },
-    {
-        name: '10',
-        logo: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
-    },
-    {
-        name: '11',
-        logo: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
-    },
-    {
-        name: '12',
-        logo: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
-    }
   ];
 
 export default () => (
     <div>
         <p style={{color: 'rgba(0,0,0,0.65)'}} className="text-4xl">Sponsors</p>
         <p className="text-2xl">A huge thank you to our awesome sponsors & partners. Coming soon!</p>
-
-        {/* <Row>
-        {
-            sponsors.map(sponsor => (
-                <Col key={sponsor.name} flex="1 1 150px">
-                    <div>
-                        <div style={{margin:'auto', maxWidth: '150px'}}>
-                            <img src={sponsor.logo}/>
-                        </div>
-                    </div>
-                    
-                </Col>
-            ))
-        }
-        </Row> */}
+        <List
+            grid={{
+            gutter: 16,
+            xs: 1,
+            sm: 2,
+            md: 3,
+            lg: 3,
+            xl: 3,
+            xxl: 3
+            }}
+            dataSource={shuffleArray(sponsors)}
+            renderItem={sponsor => (
+            <List.Item>
+                <div class="sponsor-image" 
+                    style={{
+                        backgroundPosition:'center center', 
+                        backgroundImage: `url(${sponsor.logo})`, 
+                        backgroundSize:'contain', 
+                        backgroundRepeat:'no-repeat',
+                        height:'250px',
+                        margin:'0 10px 0 10px',
+                        maxWidth:'300px'
+                    }}
+                />
+            </List.Item>
+            )}
+        />
     </div>
-    
 )
