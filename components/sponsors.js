b94dc4772675fc4ebb1cc4b7c224e548ef0e1f9d
Peter Hanssens <phanssens1@gmail.com>
2020-06-10 11:59:27 +1000

    logos and committee
    * add sponsor logos
    
    * remove coming soon
    
    * refactor sponsors
    
    * adding committee
    
    * fixed order and redorder
    
    * adding links and new yow logo
    
    Co-authored-by: lethrez <leo.hoare@live.com>

--- components/sponsors.js
+++ components/sponsors.js
@@ -1,75 +1,77 @@
-import { Row, Col, Divider} from 'antd';
+import React, { Component } from 'react';
+import { List } from 'antd';
 
-const sponsors = [
-    {
-        name: '1',
-        logo: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
-    },
-    {
-        name: '2',
-        logo: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
-    },
-    {
-        name: '3',
-        logo: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
-    },
-    {
-        name: '4',
-        logo: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
-    },
-    {
-        name: '5',
-        logo: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
-    },
-    {
-        name: '6',
-        logo: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
-    },
-    {
-        name: '7',
-        logo: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
-    },
-    {
-        name: '8',
-        logo: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
-    },
-    {
-        name: '9',
-        logo: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
-    },
-    {
-        name: '10',
-        logo: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
-    },
-    {
-        name: '11',
-        logo: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
-    },
-    {
-        name: '12',
-        logo: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
+
+export default class Sponsors extends Component {
+    constructor(props) {
+        super(props);
     }
-  ];
 
-export default () => (
-    <div>
-        <p style={{color: 'rgba(0,0,0,0.65)'}} className="text-4xl">Sponsors</p>
-        <p className="text-2xl">A huge thank you to our awesome sponsors & partners. Coming soon!</p>
-
-        {/* <Row>
+    sponsors = [
         {
-            sponsors.map(sponsor => (
-                <Col key={sponsor.name} flex="1 1 150px">
-                    <div>
-                        <div style={{margin:'auto', maxWidth: '150px'}}>
-                            <img src={sponsor.logo}/>
-                        </div>
-                    </div>
-                    
-                </Col>
-            ))
-        }
-        </Row> */}
-    </div>
-    
-)
+            name: 'snowflake',
+            logo: "./images/sponsors/snowflake.png",
+            height: "250px",
+            url: "https://www.snowflake.com",
+        },
+        {
+            name: 'mongodb',
+            logo: "./images/sponsors/mongoDB_transparent.png",
+            height: "250px",
+            url: "https://www.mongodb.com",
+        },
+        // {
+        //     name: 'fivetran',
+        //     logo: "./images/sponsors/fivetran.png",
+        // },
+        {
+            name: 'yow',
+            logo: "./images/sponsors/yow_colour_stacked.png",
+            height: "250px",
+            url: "https://yowconference.com/data/",
+        },
+        {
+            name: 'cloudshuttle',
+            logo: "./images/sponsors/cloudshuttle.png",
+            height: "250px",
+            url: "https://www.cloudshuttle.com.au",
+        },
+    ];
+
+    render() {
+        return (
+            <div>
+            <p style={{color: 'rgba(0,0,0,0.65)'}} className="text-4xl">Sponsors</p>
+            <p className="text-2xl">A huge thank you to our awesome sponsors & partners.</p>
+            <List
+                grid={{
+                gutter: 16,
+                xs: 1,
+                sm: 2,
+                md: 3,
+                lg: 3,
+                xl: 3,
+                xxl: 3
+                }}
+                dataSource={this.sponsors}
+                renderItem={sponsor => (
+                <List.Item>
+                    <a href={sponsor.url} >
+                    <div class="sponsor-image" 
+                        style={{
+                            backgroundPosition:'center center', 
+                            backgroundImage: `url(${sponsor.logo})`, 
+                            backgroundSize:'contain', 
+                            backgroundRepeat:'no-repeat',
+                            height: `${sponsor.height}`,
+                            margin:'0 10px 0 10px',
+                            maxWidth:'300px'
+                        }}
+                    />
+                    </a>
+                </List.Item>
+                )}
+            />
+        </div>
+    )}
+}
