8457238b076eab3b790ea251118fb2fa5831a4c3
Peter Hanssens <phanssens1@gmail.com>
2020-06-17 10:29:54 +1000

    removing redundant files
    * adding in TW
    
    * removing redundant files

--- pages/app.js
+++ pages/app.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Layout } from 'antd';
 import Navbar from '../components/navbar'
 import FooterClass from '../components/footer'
+import Head from 'next/head';
 
 /* tabs */ 
 import Home from './tabs/home'
@@ -43,6 +44,23 @@ export default class App extends Component {
         const { Header, Content, Footer } = Layout;
         return (
             <Layout className="layout" style={{minHeight:'100vh'}}>
+                <Head>
+                    <title>DataEngBytes - DataEngConfAU</title>
+                    <meta property="og:title" content="DataEngBytes - DataEngConfAU" key="title" />
+                    <meta name="description" content="DataEngBytes, a community conference hosted on the 20th to the 21st of August!" />
+                    <meta property="og:type" content="website" />
+                    <meta property="og:site_name" content="DataEngBytes" />
+                    <meta property="og:url" content="https://dataengconf.com.au" />  
+                    <meta name="twitter:card" content="summary" /> 
+                    <meta name="twitter:title" content="DataEngBytes" />
+                    <meta name="twitter:site" content="https://dataengconf.com.au" />
+                    <meta name="twitter:creator" content="DataEngConfAU" />
+                    <link rel="icon" type="image/png" href="/public/favicon.ico" />
+                    <link rel="apple-touch-icon" href="/public/favicon.ico" />
+                    <link rel="stylesheet" href="" />
+                    <meta property="og:image" content="" />  
+                    <meta name="twitter:image" content="" />   
+                </Head>
                 <Header theme="light" >
                     <Navbar
                         theme="dark"
