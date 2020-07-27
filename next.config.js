9775dbc4d8ff755a2def460fd95fe3c31070063d
Peter Hanssens <peter@MacBook-Pro.local>
2020-07-27 20:34:50 +1000

    fixing profile page exports

--- next.config.js
+++ next.config.js
@@ -1,2 +1,18 @@
+const getPosts = require('./data/profiles.json');
 const withImages = require('next-images')
-module.exports = withImages()
+module.exports = withImages({
+    async exportPathMap(defaultPathMap) {
+    const pathMap = {
+        '/': { page: '/index' },
+        '/code_of_conduct': { page: '/code_of_conduct' },
+        '/contact_us': { page: '/contact_us' },
+        '/organisers': { page: '/organisers' },
+      };
+
+    // now get the dynamic stuff:
+    getPosts.map(post => {
+      pathMap[`/profile/${post.id}`] = { page: '/profile/[id]' };
+    });
+    return pathMap;
+    },
+  })
