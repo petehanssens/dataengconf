ddd142636a9fba6a7e56e773d77e9e51a978c8ff
Peter Hanssens <peter@MacBook-Pro.lan>
2020-07-30 10:05:00 +1000

    adding more images

---
+++ components/navToggle.js
@@ -0,0 +1,2 @@
+export const navToggle = () => document.getElementById("nav-content").classList.toggle("hidden");
+export const navToggle2 = () => document.getElementById('nav-toggle').onclick = () => {navToggle};
