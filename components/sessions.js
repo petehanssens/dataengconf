91e2d93a526dfee98fa41fe094650f445bd5b9e6
Katie Peterson <katie.petersen@zip.co>
2020-08-04 16:04:27 +1000

    KP | WIP - session section

--- components/sessions.js
+++ components/sessions.js
@@ -1,5 +1,5 @@
-import React from 'react'; 
-import sessionData from "../data/sessions.json"; 
+import React from 'react';
+import sessionData from "../data/sessions.json";
 
 
 export default function Sessions() {
@@ -15,28 +15,83 @@ export default function Sessions() {
           {sessionData.Day1[0].StartTime}: {sessionData.Day1[0].Description}
       </div>
     </div>
+    {/* <div className="bg-gray-400 h-24 rounded flex items-center justify-center align-middle text-blue text-bold text-xl">
+      {sessionData.map(session => (
+        <a key={session.Day1.StartTime}>
+        </a>
+      ))}
+    </div> */}
     <div className="w-1/4 px-2">
-      <div className="bg-gray-400 h-24 rounded"></div>
+      <div className="bg-gray-400 h-24 rounded flex items-center justify-center align-middle text-blue text-bold text-xl">
+        {sessionData.Day1[1].StartTime}: {sessionData.Day1[1].Description}
+        <br></br>
+        {sessionData.Day1[1].SpeakerName}
+      </div>
     </div>
     <div className="w-1/4 px-2">
-      <div className="bg-gray-400 h-24 rounded"></div>
+      <div className="bg-gray-400 h-24 rounded flex items-center justify-center align-middle text-blue text-bold text-xl">
+        {sessionData.Day1[2].StartTime}: {sessionData.Day1[2].Description}
+        <br></br>
+        {sessionData.Day1[2].SpeakerName}
+      </div>
     </div>
     <div className="w-1/4 px-2">
-      <div className="bg-gray-400 h-24 rounded"></div>
+    <div className="bg-gray-400 h-24 rounded flex items-center justify-center align-middle text-blue text-bold text-xl">
+        {sessionData.Day1[3].StartTime}: {sessionData.Day1[3].Description}
+        <br></br>
+        {sessionData.Day1[3].SpeakerName}
+      </div>
+    </div>
+    <div className="w-1/4 px-2">
+      <div className="bg-gray-400 h-24 rounded flex items-center justify-center align-middle text-blue text-bold text-xl">
+        {sessionData.Day1[4].StartTime}: {sessionData.Day1[4].Description}
+        <br></br>
+        {sessionData.Day1[4].SpeakerName}
+      </div>
     </div>
   </div>
   <div className="flex -mx-2 py-4">
     <div className="w-1/4 px-2">
-      <div className="bg-gray-400 h-24 rounded"></div>
+      <div className="bg-gray-400 h-24 rounded flex items-center justify-center align-middle text-blue text-bold text-xl">
+        {sessionData.Day1[5].StartTime}: {sessionData.Day1[5].Description}
+        <br></br>
+        {sessionData.Day1[5].SpeakerName}
+      </div>
     </div>
     <div className="w-1/4 px-2">
-      <div className="bg-gray-400 h-24 rounded"></div>
+      <div className="bg-gray-400 h-24 rounded flex items-center justify-center align-middle text-blue text-bold text-xl">
+        {sessionData.Day1[6].StartTime}: {sessionData.Day1[6].Description}
+        <br></br>
+        {sessionData.Day1[6].SpeakerName}
+      </div>
     </div>
     <div className="w-1/4 px-2">
-      <div className="bg-gray-400 h-24 rounded"></div>
+    <div className="bg-gray-400 h-24 rounded flex items-center justify-center align-middle text-blue text-bold text-xl">
+        {sessionData.Day1[7].StartTime}: {sessionData.Day1[7].Description}
+        <br></br>
+        {sessionData.Day1[7].SpeakerName}
+      </div>
     </div>
     <div className="w-1/4 px-2">
-      <div className="bg-gray-400 h-24 rounded"></div>
+    <div className="bg-gray-400 h-24 rounded flex items-center justify-center align-middle text-blue text-bold text-xl">
+        {sessionData.Day1[8].StartTime}: {sessionData.Day1[8].Description}
+        <br></br>
+        {sessionData.Day1[8].SpeakerName}
+      </div>
+    </div>
+    <div className="w-1/4 px-2">
+    <div className="bg-gray-400 h-24 rounded flex items-center justify-center align-middle text-blue text-bold text-xl">
+        {sessionData.Day1[9].StartTime}: {sessionData.Day1[9].Description}
+        <br></br>
+        {sessionData.Day1[9].SpeakerName}
+      </div>
+    </div>
+    <div className="w-1/4 px-2">
+    <div className="bg-gray-400 h-24 rounded flex items-center justify-center align-middle text-blue text-bold text-xl">
+        {sessionData.Day1[10].StartTime}: {sessionData.Day1[10].Description}
+        <br></br>
+        {sessionData.Day1[10].SpeakerName}
+      </div>
     </div>
   </div>
 </div>
