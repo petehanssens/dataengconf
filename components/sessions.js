6a60ab2a87e6cb0fceb3750cf29485694871c9bc
Peter Hanssens <phanssens1@gmail.com>
2020-08-16 22:09:35 +1000

    Develop (#52)
    * adding conference timing data
    
    * Feature/sessions (#48)
    
    * KP | Update organiser headshot
    
    * KP | WIP - session section
    
    * KP | Add session data for Thursday and Friday
    
    * KP | Manipulate data to display correct time - feeling lazy and drop sessions.
    
    * KP | Update Katies speaker section to fix a typo
    
    * a few updates
    
    Co-authored-by: Katie Peterson <katie.petersen@zip.co>
    Co-authored-by: Peter Hanssens <peter@MacBook-Pro.lan>
    
    * robin typo, speaker links include talkname (#51)
    
    Co-authored-by: Peter Hanssens <peter@MacBook-Pro.lan>
    Co-authored-by: Katie Peterson <katie.petersen@zip.co>
    Co-authored-by: Leo Hoare <37860104+lethrez@users.noreply.github.com>
    Co-authored-by: Leo Hoare <leo.hoare@itoc.com.au>

--- components/sessions.js
+++ components/sessions.js
@@ -9,19 +9,16 @@ export default function Sessions() {
       <p style={{ color: "rgba(0,0,0,0.65)" }} className="text-4xl">
         Sessions
       </p>
-      <p className="text-2xl">Afternoon - Thursday 20 August (GMT+10)</p>
+      <p className="text-2xl">Afternoon - Thursday 20 August</p>
       <br></br>
-      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-2 md:gap-4 w-full">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 md:gap-4 w-full">
         {sessionData
           .filter((session) => session.day != "Friday")
           .map((session) => (
-            <div key={session.speaker_name} key={session.day} className="w-400 -mx-2 px-4">
-              <div className="bg-gray-400 w-400 h-24 rounded flex items-center justify-center align-middle text-blue text-bold text-lg"
-
-              >
-                <strong>{session.start_time_aest}:</strong> {" "}
+            <div className="w-400 -mx-2 px-4">
+              <div className="bg-gray-400 w-400 h-50 h-full p-3 rounded flex items-center align-middle text-blue text-bold text-lg">
                 <Link href={'speaker/'+session.link} >
-                  <a className="px-4">{session.speaker_name || "DataEngBytes team"}</a>
+                  <a> <strong>{session.start_time_aest}: {session.speaker_name || "DataEngBytes team"}</strong><br/>{session.talk_title}</a>
                 </Link>
               </div>
             </div>
@@ -30,18 +27,18 @@ export default function Sessions() {
 
       <br></br>
 
-      <p className="text-2xl">Morning - Friday 21 August (GMT+10)</p>
+      <p className="text-2xl">Morning - Friday 21 August</p>
       <br></br>
-      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-2 md:gap-4 w-full">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 md:gap-4 w-full">
         {sessionData
           .filter((session) => session.day != "Thursday")
           .map((session) => (
-            <div key={session.speaker_name} className="w-400 -mx-2 px-4">
-              <div className="bg-gray-400 w-400 h-24 rounded flex items-center justify-center align-middle text-blue text-bold text-lg">
-                <strong>{session.start_time_aest}:</strong>{" "}
+            <div className="w-400 -mx-2 px-4">
+              <div className="bg-gray-400 w-400 h-50 h-full p-3 rounded flex items-center align-middle text-blue text-bold text-lg">
                 <Link href={'speaker/'+session.link} >
-                  <a className="px-4">{session.speaker_name || "DataEngBytes team"}</a>
+                  <a> <strong>{session.start_time_aest}: {session.speaker_name || "DataEngBytes team"}</strong><br/>{session.talk_title}</a>
                 </Link>
+                <span>{session.TalkTitle}</span>
               </div>
             </div>
           ))}
