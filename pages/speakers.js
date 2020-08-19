ae369f55e3c64c65573660ef5d0e92b913c949ed
Peter Hanssens <phanssens1@gmail.com>
2020-08-19 15:03:16 +1000

    Develop (#54)
    * adding conference timing data
    
    * Feature/sessions (#48)
    
    * KP | Update organiser headshot
    
    * KP | WIP - session section
    
    * KP | Add session data for Thursday and Friday
    
    * KP | Manipulate data to display correct time - feeling lazy and drop sessions.
    
    * KP | Update Katies speaker section to fix a typo
    
    * a few updates
    
    Co-authored-by: Katie Peterson <katie.petersen@zip.co>
    Co-authored-by: Peter Hanssens <peter@MacBook-Pro.lan>
    
    * robin typo, speaker links include talkname (#51)
    
    * Feature/fix speakers (#53)
    
    * speaker container fix
    
    * fix speakers and highlighting for links
    
    Co-authored-by: Peter Hanssens <peter@MacBook-Pro.lan>
    Co-authored-by: Katie Peterson <katie.petersen@zip.co>
    Co-authored-by: Leo Hoare <37860104+lethrez@users.noreply.github.com>
    Co-authored-by: Leo Hoare <leo.hoare@itoc.com.au>

--- pages/speakers.js
+++ pages/speakers.js
@@ -15,22 +15,24 @@ export default function Speakers() {
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 md:gap-4 w-full">
                 { AllSpeakers.map(speaker => (
                     <Link href={speaker.link} >
-                        <div className="border border-gray-400 bg-white h-96 lg:h-auto flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden w-auto px-4 py-2" >
-                            <div className="text-gray-900 font-bold text-xl mb-2 flex justify-between">
-                                {speaker.name}
-                            </div>
-			                <div className="mx-auto lg:mx-0 w-full pt-3 border-b-2 border-black-500 opacity-75"></div>
-                            <div className="mb-8 grid grid-cols-2 object-center justify-center items-center content-center">
-                                <div className=" object-center justify-center items-center content-center">
-                                    <img src={speaker.image} alt={speaker.name} className="w-full object-center rounded-full p-4 content-center justify-center items-center" />
+                        <a>
+                            <div className="border border-gray-400 bg-white h-96 lg:h-auto flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden w-auto px-4 py-2" >
+                                <div className="text-gray-900 font-bold text-xl mb-2 flex justify-between">
+                                    {speaker.name}
                                 </div>
-                                <div className="mb-8">
-                                    <p className="text-grey-darker text-base text-left">{speaker.title}</p>
-                                    <br />
-                                    <p className="text-xs text-left font-bold">{speaker.Day} - {speaker.StartTime} (GMT+10)</p>
+                                <div className="mx-auto lg:mx-0 w-full pt-3 border-b-2 border-black-500 opacity-75"></div>
+                                <div className="mb-8 grid grid-cols-2 object-center justify-center items-center content-center">
+                                    <div className=" object-center justify-center items-center content-center">
+                                        <img src={speaker.image} alt={speaker.name} className="w-full object-center rounded-full p-4 content-center justify-center items-center" />
+                                    </div>
+                                    <div className="mb-8">
+                                        <p className="text-grey-darker text-base text-left">{speaker.title}</p>
+                                        <br />
+                                        <p className="text-xs text-left font-bold">{speaker.Day} - {speaker.StartTime} (GMT+10)</p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
+                        </a>
                     </Link>
                     ))}
                 </div>
