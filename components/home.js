9b8a0f6ee7e4e9e64b2afc794b6cb1e188eb92e5
Peter Hanssens <phanssens1@gmail.com>
2021-01-31 00:38:12 +1100

    making a start on 2021

--- components/home.js
+++ components/home.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import Sponsors from './sponsors';
-import Speakers from './speakers';
-import Keynotes from './keynotes';
-import Sessions from './sessions';
-
 
 export default function Home() {
     return (
         <>
-            <div style={{minHeight:'85vh'}}>
-                <img style={{}} className="header-image" alt="header image"/>
+            <div style={{minHeight:'85vh', 
+                backgroundColor: '#120136', 
+                color: '#FF4033'}}>
+                <img style={{}} className="data-eng-logo" alt="header image"/>
                 <br/>
                 <span className="text-4xl">
-                <p style={{textAlign: 'right'}}>Thu 20<sup>th</sup> - Fri 21<sup>st</sup> August, 2020.</p>
-                <p className="text-2xl">
+                <p style={{textAlign: 'right'}}>Thu 26<sup>th</sup> - Fri 27<sup>st</sup> August, 2021.</p>
+                <p className="text-2xl" style={{color: '#671875'}}>
                 DataEngConfAu - Bytes! Join us this year for our virtual Australian conference focusing on real world data engineering problems.
                 A not-for-profit event featuring two half day sessions jam packed with bite sized presentation with plenty of sharing and learning.
                 DataEngBytes 2020 is a virtual event - with our in-person event scheduled for 2021.
@@ -21,14 +18,6 @@ export default function Home() {
                 </span>
                 <br/>
             </div>
-
-            <Keynotes />
-
-            <Speakers  />
-
-            <Sessions />
-
-            <Sponsors  />
 
         </>
     )
