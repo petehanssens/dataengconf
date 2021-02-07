d5b7a6afabd2309a56e8d14472f62216ff5a49e6
lethrez <leo.hoare@live.com>
2021-02-07 18:25:50 +1100

    starting 2021 landing page

--- components/home.js
+++ components/home.js
@@ -4,16 +4,16 @@ export default function Home() {
     return (
         <>
             <div style={{minHeight:'85vh', 
-                backgroundColor: '#120136', 
+                // backgroundColor: '#120136', 
                 color: '#FF4033'}}>
+                <section className="hero container max-w-screen-lg mx-auto pb-10 flex justify-center">
                 <img style={{}} className="data-eng-logo" alt="header image"/>
+                </section>
                 <br/>
                 <span className="text-4xl">
-                <p style={{textAlign: 'right'}}>Thu 26<sup>th</sup> - Fri 27<sup>st</sup> August, 2021.</p>
-                <p className="text-2xl" style={{color: '#671875'}}>
-                DataEngConfAu - Bytes! Join us this year for our virtual Australian conference focusing on real world data engineering problems.
-                A not-for-profit event featuring two half day sessions jam packed with bite sized presentation with plenty of sharing and learning.
-                DataEngBytes 2020 is a virtual event - with our in-person event scheduled for 2021.
+                <p className="text-center">Thu 26<sup>th</sup> - Fri 27<sup>st</sup> August, 2021.</p>
+                <p className="text-2xl text-center data-eng-purple">
+                DataEngBytes 2021 is coming!
                 </p>
                 </span>
                 <br/>
