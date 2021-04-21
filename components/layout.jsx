117bc00a22818cf573849c31fccf78aba2e8d594
Peter Hanssens <phanssens1@gmail.com>
2021-04-21 18:47:56 +1000

    fix seo (#74)

--- components/layout.jsx
+++ components/layout.jsx
@@ -8,23 +8,6 @@ function Layout ({ children }) {
 
     return (
         <>
-            <Head>
-                <title>DataEngBytes 2021</title>
-                <meta property="og:title" content="DataEngBytes 2021" key="title" />
-                <meta name="description" content="DataEngBytes, a community conference hosted on the 26th to the 27th of August!" />
-                <meta property="og:type" content="website" />
-                <meta property="og:site_name" content="DataEngBytes" />
-                <meta property="og:url" content="https://dataengconf.com.au" />
-                <meta name="twitter:card" content="summary" />
-                <meta name="twitter:title" content="DataEngBytes" />
-                <meta name="twitter:site" content="https://dataengconf.com.au" />
-                <meta name="twitter:creator" content="DataEngConfAU" />
-                <link rel="icon" type="image/png" href="/favicon.ico" />
-                <link rel="apple-touch-icon" href="/favicon.ico" />
-                <link rel="stylesheet" href="" />
-                <meta property="og:image" content="https://dataengconf.com.au/images/logos/dataeng/bytes_center.svg" />
-                <meta name="twitter:image" content="https://dataengconf.com.au/images/logos/dataeng/bytes_center.svg" />
-            </Head>
         <Navbar />
             <body className="">
                 <main className="w-full container mx-auto p-12">
