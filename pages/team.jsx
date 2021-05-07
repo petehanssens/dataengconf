1dc650f100a39a4ce5d45e2cf4fa0e43782f8261
Peter Hanssens <phanssens1@gmail.com>
2021-05-07 23:29:50 +1000

    adding eventDetails and team breadcrumb

--- pages/team.jsx
+++ pages/team.jsx
@@ -143,13 +143,34 @@ function makePersonSchema(person) {
   }
 }
 
+const breadcrumb = {
+  "@context": "https://schema.org/", 
+  "@type": "BreadcrumbList", 
+  "itemListElement": [{
+    "@type": "ListItem", 
+    "position": 1, 
+    "name": "Home",
+    "item": "https://dataengconf.com.au/"  
+  },{
+    "@type": "ListItem", 
+    "position": 2, 
+    "name": "team",
+    "item": "https://dataengconf.com.au/team"  
+  }]
+}
+
 export default function Team() {
     return (
 
     <Layout>
+      <script 
+        key={`breadcrumbJSON`}
+        type='application/ld+json'
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(breadcrumb) }}
+      />
       {organisers.map((person) => (
     <script
-        key={`jobJSON-${person.full_name}`}
+        key={`organiserJSON-${person.full_name}`}
         type='application/ld+json'
         dangerouslySetInnerHTML={{ __html: JSON.stringify(makePersonSchema(person)) }}
     />
@@ -157,7 +178,7 @@ export default function Team() {
       }
       {committee.map((person) => (
     <script
-        key={`jobJSON-${person.full_name}`}
+        key={`committeeJSON-${person.full_name}`}
         type='application/ld+json'
         dangerouslySetInnerHTML={{ __html: JSON.stringify(makePersonSchema(person)) }}
     />
@@ -266,7 +287,7 @@ export default function Team() {
           <div className="lg:col-span-2">
             <ul className="space-y-12 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 sm:space-y-0 lg:gap-x-8">
               {committee.map((person) => (
-                <li key={person.name}>
+                <li key={person.full_name}>
                   <div className="space-y-4">
                     <div className="aspect-w-3 aspect-h-2">
                       <img className="object-cover shadow-lg rounded-lg" src={person.profile_picture} alt="" />
