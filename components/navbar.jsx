2b07c0b5fbee85fa57f14b6beda175a67873a3b9
Peter Hanssens <phanssens1@gmail.com>
2021-06-03 23:19:03 +1000

    navbar changes

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -134,17 +134,17 @@ export default function Navigation() {
                   )}
                 </Popover>
               </Popover.Group>
-            <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-              <a href="/auth" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
-                Sign in
-              </a>
-              <a
-                href="/auth"
-                className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
-              >
-                Sign up
-              </a>
-            </div>
+              <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+                <a href="/auth" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
+                  Sign in
+                </a>
+                <a
+                  href="/auth"
+                  className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+                >
+                  Sign up
+                </a>
+              </div>
             </div>
           </div>
 
