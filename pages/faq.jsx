e7b3745a77fa101c784c4328231c96662f9f0325
Peter Hanssens <phanssens1@gmail.com>
2021-06-15 13:00:42 +1000

    updated faq's

--- pages/faq.jsx
+++ pages/faq.jsx
@@ -56,36 +56,22 @@ function classNames(...classes) {
 
 export default function Example() {
   return (
-    <div className="bg-gray-50">
-      <div className="max-w-7xl mx-auto py-12 px-4 sm:py-16 sm:px-6 lg:px-8">
-        <div className="max-w-3xl mx-auto divide-y-2 divide-gray-200">
-          <h2 className="text-center text-3xl font-extrabold text-gray-900 sm:text-4xl">Frequently asked questions</h2>
-          <dl className="mt-6 space-y-6 divide-y divide-gray-200">
-            {faqs.map((faq) => (
-              <Disclosure as="div" key={faq.question} className="pt-6">
-                {({ open }) => (
-                  <>
-                    <dt className="text-lg">
-                      <Disclosure.Button className="text-left w-full flex justify-between items-start text-gray-400">
-                        <span className="font-medium text-gray-900">{faq.question}</span>
-                        <span className="ml-6 h-7 flex items-center">
-                          <ChevronDownIcon
-                            className={classNames(open ? '-rotate-180' : 'rotate-0', 'h-6 w-6 transform')}
-                            aria-hidden="true"
-                          />
-                        </span>
-                      </Disclosure.Button>
-                    </dt>
-                    <Disclosure.Panel as="dd" className="mt-2 pr-12">
-                      <p className="text-base text-gray-500">{faq.answer}</p>
-                    </Disclosure.Panel>
-                  </>
-                )}
-              </Disclosure>
-            ))}
-          </dl>
+      <div className="bg-gray-50">
+        <div className="max-w-7xl mx-auto py-12 px-4 divide-y divide-gray-200 sm:px-6 lg:py-16 lg:px-8">
+          <h2 className="text-3xl font-extrabold text-gray-900">Frequently asked questions</h2>
+          <div className="mt-8">
+            <dl className="divide-y divide-gray-200">
+              {faqs.map((faq) => (
+                <div key={faq.id} className="pt-6 pb-8 md:grid md:grid-cols-12 md:gap-8">
+                  <dt className="text-base font-medium text-gray-900 md:col-span-5">{faq.question}</dt>
+                  <dd className="mt-2 md:mt-0 md:col-span-7">
+                    <p className="text-base text-gray-500">{faq.answer}</p>
+                  </dd>
+                </div>
+              ))}
+            </dl>
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
 }
