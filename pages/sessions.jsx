71abcb5629cee422493e5ba36214cf654f2536e9
Peter Hanssens <phanssens1@gmail.com>
2021-09-30 14:02:54 +1000

    a fix for our sessions page

--- pages/sessions.jsx
+++ pages/sessions.jsx
@@ -14,8 +14,8 @@ const formatDate = (dateString) => {
 export default function Speakers() {
 
     return (
-
-      <div className="bg-white pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+<>
+    <div className="bg-white pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
       <div className="relative max-w-lg mx-auto divide-y-2 divide-gray-200 lg:max-w-7xl">
         <div>
           <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">Live Track Sessions</h2>
@@ -65,6 +65,11 @@ export default function Speakers() {
             </div>
           ))}
         </div>
+      </div>
+    </div>
+
+    <div className="bg-white pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+      <div className="relative max-w-lg mx-auto divide-y-2 divide-gray-200 lg:max-w-7xl">
         <div>
           <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">On-demand Sessions</h2>
           <p className="mt-3 text-xl text-gray-500 sm:mt-4">
@@ -114,5 +119,6 @@ export default function Speakers() {
         </div>
       </div>
     </div>
+  </>
   )
 }
