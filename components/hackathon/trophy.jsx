ec3aff14c135cc3930be48a3494df20042988358
Peter Hanssens <phanssens1@gmail.com>
2022-03-24 12:06:12 +1100

    updating trophy link

--- components/hackathon/trophy.jsx
+++ components/hackathon/trophy.jsx
@@ -89,7 +89,7 @@ export default function Example() {
                 </p>
                 <a
                   className="block w-full py-3 px-5 text-center bg-white border border-transparent rounded-md shadow-md text-base font-medium text-indigo-700 hover:bg-gray-50 sm:inline-block sm:w-auto"
-                  href="#"
+                  href="https://www.tickettailor.com/events/dataengbytes/664753"
                 >
                   Hackathon tickets
                 </a>
