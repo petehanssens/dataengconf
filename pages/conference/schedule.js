4e4d2e9b03f81718571d88d511efdad992c79336
petehanssens <phanssens1@gmail.com>
2022-08-04 10:17:01 +1000

    adding conf files

---
+++ pages/conference/schedule.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import SEO from "../../components/seo";
+import Schedule from '../../components/speakers/schedule'
+
+export default function Home() {
+  return (
+    <>
+      <SEO
+        keywords={[`Data Engineering`,`DataEng`,`DataEngBytes`]}
+        title="The schedule of DataEngBytes"
+        description="A community run Data Engineering conference in Australia and New Zealand"
+      />
+      <Schedule />
+    </>
+  )
+}
+
