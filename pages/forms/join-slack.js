333a6fe4831b1d291eb5f5fd1d03efe47175ba23
Badrivishal Iyengar <badrivishaliyengar@gmail.com>
2022-08-04 18:26:15 +1000

    Added page 2 of google form

--- pages/forms/join-slack.js
+++ pages/forms/join-slack.js
@@ -114,12 +114,133 @@ function SpeakingForm() {
                     required className="w-full border-white px-5 py-3 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-indigo-700 focus:ring-white rounded-md" 
                     placeholder="What is your LinkedIn Profile" />
                   <label className="sr-only">LinkedIn Profile</label>
-                  
+
+                  <label>What warehouse do you use</label>
+                  <label for="redshift">
+                  <input type="checkbox" id="redshift" name="redshift" value="yes"/> Redshift 
+                  </label>
+                  <label for="snowflake">
+                  <input type="checkbox" id="snowflake" name="snowflake" value="yes"/> Snowflake 
+                  </label> 
+                  <label for="s3">
+                  <input type="checkbox" id="s3" name="s3" value="yes"/> S3 
+                  </label>
+                  <label for="emr">
+                  <input type="checkbox" id="emr" name="emr" value="yes"/> EMR 
+                  </label>
+                  <label for="big_query">
+                  <input type="checkbox" id="big_query" name="big_query" value="yes"/> Big Query 
+                  </label>
+                  <label for="azure_data_warehouse">
+                  <input type="checkbox" id="azure_data_warehouse" name="azure_data_warehouse" value="yes"/> Azure Data Warehouse
+                  </label>
+                  <label for="other">
+                  <input type="checkbox" id="other" name="other" value="yes"/> Other 
+                  </label>
+
+                  <label>What languages you use to do data engineering</label>
+                  <label for="python">
+                  <input type="checkbox" id="python" name="python" value="yes"/> Python 
+                  </label>
+                  <label for="scala">
+                  <input type="checkbox" id="scala" name="scala" value="yes"/> Scala 
+                  </label>
+                  <label for="java">
+                  <input type="checkbox" id="java" name="java" value="yes"/> Java 
+                  </label>
+                  <label for="go">
+                  <input type="checkbox" id="go" name="go" value="yes"/> Go 
+                  </label>
+                  <label for="node.js">
+                  <input type="checkbox" id="node.js" name="node.js" value="yes"/> Node.js 
+                  </label>
+                  <label for="r">
+                  <input type="checkbox" id="r" name="r" value="yes"/> R 
+                  </label>
+                  <label for="other">
+                  <input type="checkbox" id="other" name="other" value="yes"/> Other 
+                  </label>
+
+                  <label>What frameworks do you use to do data engineering?</label>
+                  <label for="airflow">
+                  <input type="checkbox" id="airflow" name="airflow" value="yes"/> Airflow 
+                  </label>
+                  <label for="luigi">
+                  <input type="checkbox" id="luigi" name="luigi" value="yes"/> Luigi 
+                  </label>
+                  <label for="spark">
+                  <input type="checkbox" id="spark" name="spark" value="yes"/> Spark 
+                  </label>
+                  <label for="dbt">
+                  <input type="checkbox" id="dbt" name="dbt" value="yes"/> dbt 
+                  </label>
+                  <label for="kafka">
+                  <input type="checkbox" id="kafka" name="kafka" value="yes"/> Kafka 
+                  </label>
+                  <label for="other">
+                  <input type="checkbox" id="other" name="other" value="yes"/> Other 
+                  </label>
+
+                  <label>What clouds do you use</label>
+                  <label for="aws">
+                  <input type="checkbox" id="aws" name="aws" value="yes"/> AWS 
+                  </label>
+                  <label for="gcp">
+                  <input type="checkbox" id="gcp" name="gcp" value="yes"/> GCP 
+                  </label>
+                  <label for="azure">
+                  <input type="checkbox" id="azure" name="azure" value="yes"/> Azure 
+                  </label>
+                  <label for="oracle">
+                  <input type="checkbox" id="oracle" name="oracle" value="yes"/> Oracle 
+                  </label>
+                  <label for="alicloud">
+                  <input type="checkbox" id="alicloud" name="alicloud" value="yes"/> AliCloud 
+                  </label>
+                  <label for="onprem">
+                  <input type="checkbox" id="onprem" name="onprem" value="yes"/> On Prem 
+                  </label>
+                  <label for="other">
+                  <input type="checkbox" id="other" name="other" value="yes"/> Other 
+                  </label>
+
+                  <label>What vendors do you use</label>
+                  <label for="confluent">
+                  <input type="checkbox" id="confluent" name="confluent" value="yes"/> Confluent 
+                  </label>
+                  <label for="databricks">
+                  <input type="checkbox" id="databricks" name="databricks" value="yes"/> Databricks 
+                  </label>
+                  <label for="fivetran">
+                  <input type="checkbox" id="fivetran" name="fivetran" value="yes"/> fivetran 
+                  </label>
+                  <label for="matillion">
+                  <input type="checkbox" id="matillion" name="matillion" value="yes"/> Matillion 
+                  </label>
+                  <label for="cloudera">
+                  <input type="checkbox" id="cloudera" name="cloudera" value="yes"/> cloudera 
+                  </label>
+                  <label for="mapr">
+                  <input type="checkbox" id="mapr" name="mapr" value="yes"/> Mapr 
+                  </label>
+                  <label for="pentaho">
+                  <input type="checkbox" id="pentaho" name="pentaho" value="yes"/> Pentaho 
+                  </label>
+                  <label for="talend">
+                  <input type="checkbox" id="talend" name="talend" value="yes"/> Talend 
+                  </label>
+                  <label for="other">
+                  <input type="checkbox" id="other" name="other" value="yes"/> Other 
+                  </label>
                   <button type="submit" className="mt-3 w-full flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-500 hover:bg-indigo-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-indigo-700 focus:ring-white sm:mt-0 sm:ml-3 sm:w-auto sm:flex-shrink-0">
                     Submit
                   </button>
                 </form>
                 )}
+                <p className="mt-3 text-sm text-indigo-100">
+                I can confirm I have read and agree to the Data Engineers Code of Conduct which can be found here:
+                  <a href="https://sydneydataengineers.github.io/code-of-conduct.html" className="text-white font-medium underline"> Code of Conduct </a>
+                </p>
                 <p className="mt-3 text-sm text-indigo-100">
                   We care about the protection of your data. Read our
                   <a href="/privacy" className="text-white font-medium underline"> Privacy Policy. </a>
