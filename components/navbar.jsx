a96a72eb816630086f1147f29159f53d0f4cc068
petehanssens <phanssens1@gmail.com>
2022-08-15 18:25:55 +1000

    get tickets

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -324,10 +324,10 @@ export default function Navigation(props) {
               <>
               <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
                 <a
-                  href="/auth"
+                  href="https://www.tickettailor.com/events/dataengbytes"
                   className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
                 >
-                  Sign Up
+                  Get Tickets
                 </a>
               </div>
 
@@ -414,10 +414,10 @@ export default function Navigation(props) {
                         :
                     <div className="mt-6">
                       <a
-                        href="/auth"
+                        href="https://www.tickettailor.com/events/dataengbytes"
                         className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
                       >
-                        Sign Up
+                        Get Tickets
                       </a>
                     </div>
                     }
