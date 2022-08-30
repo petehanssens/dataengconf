e95f64e5eb0d093e91e10b5fed3db6590e320e66
Gunjan Chaudhari <gunjan@cloudshuttle.com.au>
2022-08-30 16:47:33 +1000

    updating-auth

--- graphql/users/createInitialUser.js
+++ graphql/users/createInitialUser.js
@@ -1,24 +1,28 @@
-import gql from 'graphql-tag'
+import gql from "graphql-tag";
 
 const mutation = gql`
-mutation (
-  $email: String!,
-  $first_name: String,
-  $last_name: String,
-  $linkedin: String,
-  $preferred_meetup: String,
-  $country: String
-) {
-createInitialUser (
-  email: $email,
-  first_name: $first_name,
-  last_name: $last_name,
-  linkedin: $linkedin,
-  preferred_meetup: $preferred_meetup,
-  country: $country
-) {
-  email
-}
-}
-`
+  mutation (
+    $email: String!
+    $first_name: String
+    $last_name: String
+    $bio: String
+    $company_name: String
+    $linkedin: String
+    $preferred_meetup: String
+    $country: String
+  ) {
+    createInitialUser(
+      email: $email
+      first_name: $first_name
+      last_name: $last_name
+      bio: $bio
+      company_name: $company_name
+      linkedin: $linkedin
+      preferred_meetup: $preferred_meetup
+      country: $country
+    ) {
+      email
+    }
+  }
+`;
 export default mutation;
