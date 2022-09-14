85121435729bc0f9ba1a514db8941df85cddc2f8
Gunjan Chaudhari <111568941+chaudharigunjan@users.noreply.github.com>
2022-09-14 22:29:42 +0530

    minor-changes (#261)

--- pages/conference/sydney.js
+++ pages/conference/sydney.js
@@ -2,7 +2,6 @@ import { Fragment } from "react";
 import SEO from "../../components/seo";
 import Sydney from "../../components/venues/sydney";
 import SydneyPanel from "../../components/speakers/sydney-panel";
-
 export default function Home() {
   return (
     <Fragment>
@@ -13,6 +12,25 @@ export default function Home() {
       />
       <Sydney />
       <SydneyPanel />
+      <div className="ml-5 m-h-60">
+        <div className="justify-center flex w-full p-5 ">
+          <img
+            className="rounded-lg object-contain mx-2 shadow-lg h-44 hover:scale-150 transition delay-450"
+            src="/images/venues/terrace-mca.jpg"
+            alt=""
+          />
+          <img
+            className="rounded-lg object-contain mx-2 shadow-lg h-44 hover:scale-150 transition delay-450"
+            src="/images/venues/foundation-hall.jpg"
+            alt=""
+          />
+          <img
+            className="rounded-lg object-contain mx-2 shadow-lg h-44 hover:scale-150 transition delay-450"
+            src="/images/venues/wesley-conference-centre.jpeg"
+            alt=""
+          />
+        </div>
+      </div>
     </Fragment>
   );
 }
