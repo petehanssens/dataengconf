b1627d883c8eefe0007aa5f714f93eba455adcf5
Gunjan Chaudhari <gunjan@cloudshuttle.com.au>
2022-09-22 08:39:07 +0530

    firstcommit

--- pages/conference/melbourne.js
+++ pages/conference/melbourne.js
@@ -17,7 +17,7 @@ export default function Home() {
       {/* <CitySessions />
       <CitySpeakers /> */}
       <MelbournePanel />
-      <div className="mx-5 m-h-40">
+      {/* <div className="mx-5 m-h-40">
         <div className="justify-center flex w-full p-5 ">
           <img
             className="rounded-lg object-contain mx-2 shadow-lg h-44 hover:scale-150 transition delay-450"
@@ -34,6 +34,66 @@ export default function Home() {
             src="/images/venues/state-library.jpeg"
             alt=""
           />
+        </div>
+      </div> */}
+      <h3 className="text-center mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+        Venue Details
+      </h3>
+      <div className="">
+        <div className="relative bg-white py-5">
+          <div className="lg:inset-0">
+            <div className="lg:inset-y-0">
+              <img
+                className="w-full lg:h-full"
+                src="/images/venues/statelibrary.png"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+        <div className="relative bg-white py-5">
+          <div className="lg:inset-0">
+            <div className="lg:inset-y-0">
+              <img
+                className=" w-full lg:h-full"
+                src="/images/venues/vic_state_lib_20191206_192350.jpeg"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+        <div className="relative bg-white py-5">
+          <div className="lg:inset-0">
+            <div className="lg:inset-y-0">
+              <img
+                className=" w-full lg:h-full"
+                src="/images/venues/village-roadshow-theatrette.jpeg"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+        <div className="relative bg-white py-5">
+          <div className="lg:inset-0">
+            <div className="lg:inset-y-0">
+              <img
+                className=" w-full lg:h-full"
+                src="/images/venues/sl-sponsor.png"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+        <div className="relative bg-white py-5">
+          <div className="lg:inset-0">
+            <div className="lg:inset-y-0">
+              <img
+                className=" w-full lg:h-full"
+                src="/images/venues/how2get-there-vic-state-lib.png"
+                alt=""
+              />
+            </div>
+          </div>
         </div>
       </div>
     </Fragment>
--- pages/conference/sydney.js
+++ pages/conference/sydney.js
@@ -12,7 +12,7 @@ export default function Home() {
       />
       <Sydney />
       <SydneyPanel />
-      <div className="ml-5 m-h-60">
+      {/* <div className="ml-5 m-h-60">
         <div className="justify-center flex w-full p-5 ">
           <img
             className="rounded-lg object-contain mx-2 shadow-lg h-44 hover:scale-150 transition delay-450"
@@ -29,6 +29,66 @@ export default function Home() {
             src="/images/venues/wesley-conference-centre.jpeg"
             alt=""
           />
+        </div>
+      </div> */}
+      <h3 className="text-center mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+        Venue Details
+      </h3>
+      <div className="">
+        <div className="relative bg-white py-5">
+          <div className="lg:inset-0">
+            <div className="lg:inset-y-0">
+              <img
+                className="w-full lg:h-full"
+                src="/images/venues/sydney-venue.jpg"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+        <div className="relative bg-white py-5">
+          <div className="lg:inset-0">
+            <div className="lg:inset-y-0">
+              <img
+                className=" w-full lg:h-full"
+                src="/images/venues/foundation-hall.jpg"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+        <div className="relative bg-white py-5">
+          <div className="lg:inset-0">
+            <div className="lg:inset-y-0">
+              <img
+                className=" w-full lg:h-full"
+                src="/images/venues/terrace-mca.jpg"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+        <div className="relative bg-white py-5">
+          <div className="lg:inset-0">
+            <div className="lg:inset-y-0">
+              <img
+                className=" w-full lg:h-full"
+                src="/images/venues/mca-sponsor.png"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+        <div className="relative bg-white py-5">
+          <div className="lg:inset-0">
+            <div className="lg:inset-y-0">
+              <img
+                className=" w-full lg:h-full"
+                src="/images/venues/mca-location.png"
+                alt=""
+              />
+            </div>
+          </div>
         </div>
       </div>
     </Fragment>
