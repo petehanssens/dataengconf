99f2e77fda3d7fb0acac1f89cce09cbb368d6838
Jingyi Chen <jingyi@cloudshuttle.com.au>
2022-09-26 11:20:09 +1000

    scandata

--- pages/api/putQRcode.js
+++ pages/api/putQRcode.js
@@ -1,13 +1,18 @@
 import { API } from 'aws-amplify';
 import scanQRcode from '../../graphql/users/scanQRcode';
 
-export default async (_, res) => {
-  try {
-    const postData = await API.graphql({ query: scanQRcode });
-    res.json({ posts: postData.data.scanQRcode});
-    console.log(postData);
-  } catch (err) {
-    console.log(err);
-    res.json({ error: err });
-  } 
+export default async (scanData, res) => {
+    try {
+        const postData = await API.graphql({
+            query: scanQRcode,
+            variables: {
+                qr_code: scanData.text
+            }
+        });
+        res.json({ posts: postData.data.scanQRcode });
+        console.log(postData);
+    } catch (err) {
+        console.log(err);
+        res.json({ error: err });
+    }
 }
