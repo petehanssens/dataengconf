ff232884094b7d1217a3c5bb58374da690fd196c
Gunjan Chaudhari <111568941+chaudharigunjan@users.noreply.github.com>
2022-09-26 05:33:14 +0530

    feat: Qrreader (#266)
    * first-commit
    
    * qrscan
    
    * backcamera
    
    * qrscan
    
    * qrscanback
    
    * qrscanrear

---
+++ pages/scan.js
@@ -0,0 +1,62 @@
+// versi "react-qr-reader" 1.0.0. component API harus disesuaikan dengan yg baru
+
+import { useState } from "react";
+import QrReader from "react-qr-scanner";
+
+const QRscan = () => {
+  const [selected, setSelected] = useState("rear");
+  const [startScan, setStartScan] = useState(false);
+  const [loadingScan, setLoadingScan] = useState(false);
+  const [data, setData] = useState("");
+
+  const handleScan = async (scanData) => {
+    setLoadingScan(true);
+    console.log(`loaded data data`, scanData, selected);
+    if (scanData && scanData !== "") {
+      console.log(`loaded >>>`, scanData, selected);
+      setData(scanData);
+      setStartScan(false);
+      setLoadingScan(false);
+      // setPrecScan(scanData);
+    }
+  };
+  const handleError = (err) => {
+    console.error(err);
+  };
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>
+        Last Scan:
+        {selected}
+      </h2>
+
+      <button
+        onClick={() => {
+          setStartScan(!startScan);
+        }}
+      >
+        {startScan ? "Stop Scan" : "Start Scan"}
+      </button>
+      {startScan && (
+        <>
+          <select onChange={(e) => setSelected(e.target.value)}>
+            <option value={"environment"}>Back Camera</option>
+            <option value={"rear"}>Front Camera</option>
+          </select>
+          <QrReader
+            facingMode={selected}
+            delay={1000}
+            onError={handleError}
+            onScan={handleScan}
+            // chooseDeviceId={()=>selected}
+            style={{ width: "300px" }}
+          />
+        </>
+      )}
+      {loadingScan && <p>Loading</p>}
+    </div>
+  );
+};
+
+export default QRscan;
