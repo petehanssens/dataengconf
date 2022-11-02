f5b43bbcd22387678fbeacfb75d03da9b235fa4e
Gunjan Chaudhari <gunjan@cloudshuttle.com.au>
2022-11-02 14:42:26 +1100

    solidbutton

--- components/home.jsx
+++ components/home.jsx
@@ -23,7 +23,7 @@ export default function Home() {
               <div className="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
                 <a
                   href="/forms/meetup-talk"
-                  className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-50 md:py-4 md:text-lg md:px-10"
+                  className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-800 md:py-4 md:text-lg md:px-10"
                 >
                   Register your Interest
                 </a>
