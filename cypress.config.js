99a168c6d7a5dc70be178c288036331f6e917a69
Gunjan Chaudhari <gunjan@cloudshuttle.com.au>
2022-11-04 11:58:02 +1100

    cypressconfig

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,23 @@
+// const { defineConfig } = require("cypress");
+
+// module.exports = defineConfig({
+//   e2e: {
+//     supportFile: false,
+//     video: false,
+//     screenshotOnRunFailure: false,
+//     setupNodeEvents(on, config) {
+//       // implement node event listeners here
+//     },
+//   },
+// });
+
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    supportFile: false,
     video: false,
     screenshotOnRunFailure: false,
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: "http://localhost:3000",
+    specPattern: ["cypress/e2e/**/*.*.js", "cypress/integration/**/*.*.js"],
   },
 });
