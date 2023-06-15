e102499b0906114bff02ee4966c816f59f6398d3
petehanssens <phanssens1@gmail.com>
2023-06-15 11:12:48 +1000

    adding more components

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				openSans: ['Open+Sans'],
+				montserrat: ['Montserrat'],
+				roboto: ['Roboto']
+			}
+		},
 	},
 	plugins: [],
 }
