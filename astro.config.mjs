5fa4b8846e5d79b4c8000a353fe6a0d31183106d
chaudharigunjan <gunjan@cloudshuttle.com.au>
2023-06-20 09:55:46 +1000

    fix: add images

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from 'astro/config';
 import image from '@astrojs/image';
 import tailwind from '@astrojs/tailwind';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image()],
+  integrations: [tailwind(), image(), sitemap()]
 });
