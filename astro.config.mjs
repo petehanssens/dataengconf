bff83ca8103a81d1dc1e37171ae5ee96eec21a69
chaudharigunjan <gunjan@cloudshuttle.com.au>
2023-06-19 10:39:31 +1000

    fix image path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
-
-import tailwind from "@astrojs/tailwind";
+import image from '@astrojs/image';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), image()],
 });
