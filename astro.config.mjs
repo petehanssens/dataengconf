35d889ebe01e2d62dc39f6810a6551ada21ddd3c
Gunjan Chaudhari <111568941+chaudharigunjan@users.noreply.github.com>
2023-06-20 10:25:10 +1000

    Fiximages (#288)
    * fix: add images
    
    * fix: images
    
    * fix: image size and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,10 @@ import { defineConfig } from 'astro/config';
 import image from '@astrojs/image';
 import tailwind from '@astrojs/tailwind';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image()],
+  site: 'https://dataengconf.com.au',
+  integrations: [tailwind(), image(), sitemap()],
 });
