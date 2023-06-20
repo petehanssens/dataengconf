4eac7a1b81ca17e8e7210028da2ba50610c8c49c
chaudharigunjan <gunjan@cloudshuttle.com.au>
2023-06-20 10:21:49 +1000

    fix: image size and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@ import { defineConfig } from 'astro/config';
 import image from '@astrojs/image';
 import tailwind from '@astrojs/tailwind';
 
-import sitemap from "@astrojs/sitemap";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image(), sitemap()]
+  site: 'https://dataengconf.com.au',
+  integrations: [tailwind(), image(), sitemap()],
 });
