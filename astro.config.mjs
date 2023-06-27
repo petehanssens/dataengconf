fc10b77430539cf412e83932d2b0807d64bde3fd
Gunjan Chaudhari <111568941+chaudharigunjan@users.noreply.github.com>
2023-06-27 16:51:41 +1000

    feat: Drill (#297)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
 import { defineConfig } from 'astro/config';
 import image from '@astrojs/image';
 import tailwind from '@astrojs/tailwind';
-
 import sitemap from '@astrojs/sitemap';
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'hybrid',
   site: 'https://dataengconf.com.au',
   integrations: [tailwind(), image({
     serviceEntryPoint: '@astrojs/image/sharp'
   }), sitemap()],
-  routes: {
-    "/sessions/:sessionid": "./src/pages/conference/session.astro"
-  }
+  adapter: netlify(),
+
 });
