bace47f9e544d718181a932ad367c2879c234c0a
chaudharigunjan <gunjan@cloudshuttle.com.au>
2023-06-27 14:36:56 +1000

    revertback

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  // output: 'hybrid',
+  output: 'hybrid',
   site: 'https://dataengconf.com.au',
   integrations: [tailwind(), image({
     serviceEntryPoint: '@astrojs/image/sharp'
